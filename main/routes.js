0e516e74f4c90b7b4a0a8f0849e29a662a7f5310
Carlin Au <carlinau@gmail.com>
2017-03-16 01:35:23 -0400

    added port for ip from client

--- main/routes.js
+++ main/routes.js
@@ -6,7 +6,7 @@ const init = function RouteHandler(app) {
     console.log('req.ip: ' + req.ip);
     console.log('req.hostname: ' + req.hostname);
     request.get({
-      url: req.ip+'/status'
+      url: req.ip+':8080/status'
     }, (error, response, body)=>{
       console.log("Response from %s: %s", req.ip, body);
     });
